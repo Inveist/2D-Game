ec416045238f6820f848ddddb5da7778e30e5bec
Inveist <178681096+Inveist@users.noreply.github.com>
2025-01-05 17:02:24 +0100

    Added dashing to the player character (done on my own with minor google searches)

--- 2DGame/Assets/Scripts/Player/Core/PlayerMovement.cs
+++ 2DGame/Assets/Scripts/Player/Core/PlayerMovement.cs
@@ -1,9 +1,13 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed;
+    public float dashForce;
+    [SerializeField] private bool canDash = true;
+    private bool isDashing = false;
     private Rigidbody2D rb;
     [HideInInspector] public float lastHorizontalVector;
     [HideInInspector] public float lastVerticalVector;
@@ -24,6 +28,7 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
         Move();
+        Dash();
     }
 
     void InputManagement()
@@ -47,5 +52,32 @@ public class PlayerMovement : MonoBehaviour
     void Move()
     {
         rb.linearVelocity = new Vector2(moveDir.x * moveSpeed, moveDir.y * moveSpeed);
+    }
+
+    // ReSharper disable Unity.PerformanceAnalysis
+    void Dash()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && canDash)
+        {
+            rb.AddForce(moveDir * dashForce, ForceMode2D.Impulse);
+            if (moveDir == Vector2.zero)
+            {
+                Debug.LogWarning("MoveDir is zero.");
+            }
+            canDash = false;
+        }
+        else if (!canDash && !isDashing)
+        {
+            StartCoroutine(CanDash(2));
+        }
+    }
+    
+    IEnumerator CanDash(float waitTime)
+    {
+        isDashing = true;
+        yield return new WaitForSecondsRealtime(waitTime);
+        canDash = true;
+        isDashing = false;
+        yield break;
     }
 }
